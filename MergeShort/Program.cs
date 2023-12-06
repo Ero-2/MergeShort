befd9d35a318f9f50416ae8814e800ecf3826642
Ero-2 <125475081+Ero-2@users.noreply.github.com>
2023-12-05 23:57:55 -0600

    Agregar archivos de proyecto.

---
+++ MergeShort/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeShort
+{
+    internal class Program
+    {
+        public static void MergeSort(int[] arr)
+        {
+            if (arr.Length < 2)
+                return;
+
+            int mid = arr.Length / 2;
+            int[] left = new int[mid];
+            int[] right = new int[arr.Length - mid];
+
+            Array.Copy(arr, 0, left, 0, mid);
+            Array.Copy(arr, mid, right, 0, arr.Length - mid);
+
+            MergeSort(left);
+            MergeSort(right);
+            Merge(arr, left, right);
+        }
+
+        public static void Merge(int[] arr, int[] left, int[] right)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    arr[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < left.Length)
+            {
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+
+            while (j < right.Length)
+            {
+                arr[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 2, 8, 3, 1 };
+
+            MergeSort(arr);
+
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
